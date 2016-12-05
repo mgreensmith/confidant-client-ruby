bf992760cc669c20c253d5114b06aac0063fb315
Matt Greensmith <matt.greensmith@gmail.com>
2016-12-04 21:14:23 -0800

    Add specs for Confidant module workflow. 100% coverage!

--- spec/confidant_spec.rb
+++ spec/confidant_spec.rb
@@ -1,22 +1,40 @@
 require 'spec_helper'
 
 describe Confidant do
+  before(:each) do
+    if Confidant.instance_variable_defined? :@configurator
+      Confidant.remove_instance_variable :@configurator
+    end
+  end
+
   it 'has a version number' do
     expect(Confidant::VERSION).not_to be nil
   end
 
   context 'module workflow' do
-    it 'exposes #configure' do
-      expect(Confidant).to respond_to(:configure).with(0..1).arguments
+    before(:each) do
+      stub_const('Confidant::Configurator::MANDATORY_CONFIG_KEYS', global: [])
     end
 
-    it 'exposes #get_service' do
-      expect(Confidant).to respond_to(:configure).with(0..1).arguments
+    it 'creates a module instance of Configurator' do
+      expect { Confidant.configure }.to_not raise_error
     end
-  end
 
-  it 'has a #log_exception helper' do
-    expect(Confidant).to respond_to(:log_exception).with(2).arguments
+    context '#get_service' do
+      it 'raises if not configured' do
+        expect { Confidant.get_service }
+          .to raise_error(Confidant::ConfigurationError,
+                          /Not configured, run Confidant.configure/)
+      end
+
+      it 'returns a Client response' do
+        allow_any_instance_of(Confidant::Client)
+          .to receive(:get_service).and_return(it: 'worked')
+
+        Confidant.configure
+        expect(Confidant.get_service).to eq(it: 'worked')
+      end
+    end
   end
 end
 
